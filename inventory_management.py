848acb12f9894980562ff6e4328c2df1806bb0d5
caelummmuhhh <115360461+caelummmuhhh@users.noreply.github.com>
2024-11-02 20:23:40 -0400

    complete and test order_processing and remove unnecessary functions from inventory management

--- inventory_management.py
+++ inventory_management.py
@@ -1,9 +1,10 @@
 import sqlalchemy as sa
 import pandas as pd
 from database_provider import DatabaseProvider
+from toasterdb_orms import *
 
 class InventoryManager(object):
-    """Handles Inventory."""
+    """Handles Inventory management related requests."""
     _db: sa.engine.Engine
 
     def __init__(self, db_engine: sa.engine.Engine):
@@ -30,9 +31,9 @@ class InventoryManager(object):
         pandas.Dataframe
             A DataFrame of the data retrieved.
         """
-        sql = 'SELECT * FROM INVENTORY'
+        sql = sa.select(Inventory)
         if only_in_stock:
-            sql = 'SELECT * FROM INVENTORY WHERE STOCK_QUANTITY > 0' 
+            sql = sa.select(Inventory).where(Inventory.stock_quantity > 0)
         return DatabaseProvider.pandas_read_sql(self._engine, sql)
 
 
@@ -50,9 +51,8 @@ class InventoryManager(object):
         pandas.DataFrame
             A DataFrame containing the item found based on ID, if any.
         """
-        sql = 'SELECT * FROM INVENTORY WHERE ITEM_ID = %s'
-        params = (id,)
-        return DatabaseProvider.pandas_read_sql(self._engine, sql, params=params)
+        sql = sa.select(Inventory).where(Inventory.item_id == id)
+        return DatabaseProvider.pandas_read_sql(self._engine, sql)
 
     def get_item_by_name(self, name: str) -> pd.DateOffset:
         """
@@ -68,9 +68,8 @@ class InventoryManager(object):
         pandas.DataFrame
             A DataFrame containing the item found based on the item name, if any.
         """
-        sql = 'SELECT * FROM INVENTORY WHERE UPPER(ITEM_NAME) = UPPER(%s)'
-        params = (name,)
-        return DatabaseProvider.pandas_read_sql(self._engine, sql, params=params)
+        sql = sa.select(Inventory).where(Inventory.item_name == name)
+        return DatabaseProvider.pandas_read_sql(self._engine, sql)
 
     def item_enough_stock(self, item_id: int | str, quantity: int | str) -> bool:
         """
@@ -89,44 +88,9 @@ class InventoryManager(object):
             Returns true if item stock >= quantity, false if not.
             Returns nothing if item not found.
         """
-        sql = 'SELECT * FROM INVENTORY WHERE ITEM_ID = %s AND STOCK_QUANTITY >= %s'
-        params = (item_id, quantity,)
-        df = DatabaseProvider.pandas_read_sql(self._engine, sql, params=params)
+        sql = sa.select(Inventory).where(
+            (Inventory.item_id == item_id) & (Inventory.stock_quantity >= quantity)
+        )
+        df = DatabaseProvider.pandas_read_sql(self._engine, sql)
         return not df.empty
     
-
-    def purchase_item(self, item_id: int, quantity: int):
-        """
-        Removes certain item quantity from stock. If not enough quantity, does nothing.
-
-        Parameters
-        ----------
-        item_id : int
-            The ID of the item to remove quantity of stock from
-        quantity : int
-            The quantity of stock to remove from specified item's stock quantity
-        """
-
-        sql = '''
-            UPDATE INVENTORY
-            SET STOCK_QUANTITY = STOCK_QUANTITY - :quantity
-            WHERE ITEM_ID = :id AND STOCK_QUANTITY >= :quantity
-            '''
-        params = {
-            'quantity': quantity,
-            'id': item_id
-        }
-        DatabaseProvider.query_db(self._engine, sql, params)
-
-    def purchase_items(self, items: list):
-        """
-        Remove specified quantities from each item, if enough stock, if not, does nothing for that item.
-        Expected items list format:
-        items = [
-            {
-            'id': int,
-            'quantity': int
-            }
-        ]
-        """
-        pass
--- order_processing.py
+++ order_processing.py
@@ -1,13 +1,11 @@
-import pandas as pd
-import sqlalchemy as sa
-from sqlalchemy.orm import Session, sessionmaker
-from inventory_management import InventoryManager
-from database_provider import DatabaseProvider
-from toasterdb_orms import *
 from enum import Enum
 
+import pandas as pd
+import sqlalchemy as sa
+from sqlalchemy.orm import Session
 
-### NOTE: NONE OF THIS NEW ORM CODE HAS BEEN FULLY TESTED!!
+from database_provider import DatabaseProvider
+from toasterdb_orms import *
 
 class OrderStatus(Enum):
     RECEIVED = 'Received'
@@ -19,7 +17,6 @@ class OrderStatus(Enum):
 
 class OrderProcessor(object):
     """Handles order processing for a singular order."""
-    _inventory: InventoryManager
     _engine: sa.engine.Engine
 
     _raw_order: dict
@@ -33,35 +30,144 @@ class OrderProcessor(object):
     _shipping_info_id: int = None
     _payment_info_id: int = None
 
-    def __init__(self, inventory: InventoryManager, engine: sa.engine.Engine):
-        self._inventory = inventory
+    def __init__(self, engine: sa.engine.Engine):
+        """
+        Parameters
+        ----------
+        db_engine : SQLAlchemy.engine.Engine
+            The engine to connect to the database to handle the order.
+        """
         self._engine = engine
 
-    def process_order(self, order) -> int:
+    def process_order(self, order: dict) -> tuple[int, str]:
+        """
+        Processes an order with the ordered items, payment info, and shipping info.
+        Updates the database as neccessary.
+
+        Parameters
+        ----------
+        order : dict
+            The order that's to be processed. Expected format of the order:
+            ```
+            {
+                'items': [
+                    {
+                        'item_id': 123,
+                        'quantity': 123
+                    }
+                ],
+                'payment_info': {
+                    'name': 'Jane Doe',
+                    'card_number': '123',
+                    'expiration_date': '10/20',
+                    'cvv': 123,
+                    'billing_address': {
+                        'address_1': '123 Main St',
+                        'address_2': '321 2nd Ave',
+                        'city': 'Columbus',
+                        'state': 'OH',
+                        'zip': 43210
+                    }
+                },
+                'shipping_info': {
+                    'name': 'Jane Doe',
+                    'address_1': '123 Main St',
+                    'address_2': '321 2nd Ave',
+                    'city': 'Columbus',
+                    'state': 'OH',
+                    'zip': 43210
+                }
+            }
+            ```
+        
+        Returns
+        -------
+        tuple[int, str]
+            A an HTTP response status code and a message.
+        """
         self._raw_order = order
 
-        if not self.__in_stock__(order['items']):
+        if not self.__validate_raw_order__():
+            return 400, 'Invalid order format.' # Bad Request
+
+        if not self.__in_stock__():
             # Item not in stock, return -1 to indicate it
-            return -1
-
-        self.__process_payment__()
-        self.__process_shipping__()
-
-        self._order_id = DatabaseProvider.query_db(
-            self._engine,
-            sa.select(sa.func.ifnull(sa.func.max(CustomerOrder.id), 0) + 1)
-        )[0][0]
+            return 409, 'Not enough items in stock.' # Conflict
         
-        self._order_df = pd.DataFrame({
-            CustomerOrder.id.name: [self._order_id],
-            CustomerOrder.customer_name.name: [order['payment_info']['name']],
-            CustomerOrder.shipping_info_id.name: [self._shipping_info_id],
-            CustomerOrder.payment_info_id.name: [self._payment_info_id],
-            CustomerOrder.status.name: [OrderStatus.RECEIVED.value]
-        })
-        self.__process_order_items__()
+        try:
+            self.__process_payment__()
+            self.__process_shipping__()
+
+            self._order_id = DatabaseProvider.query_db(
+                self._engine,
+                sa.select(sa.func.ifnull(sa.func.max(CustomerOrder.id), 0) + 1)
+            )[0][0]
+            
+            self._order_df = pd.DataFrame({
+                CustomerOrder.id.name: [self._order_id],
+                CustomerOrder.customer_name.name: [order['payment_info']['name']],
+                CustomerOrder.shipping_info_id.name: [self._shipping_info_id],
+                CustomerOrder.payment_info_id.name: [self._payment_info_id],
+                CustomerOrder.status.name: [OrderStatus.RECEIVED.value]
+            })
+
+            self.__process_order_items__()
+            self.__update_database_with_order__()
+        except Exception as err:
+            return 500, f'An error occurred when processing order. {type(err).__name__}'
+    
+    def __validate_raw_order__(self) -> bool:
+        """
+        Validates if the raw order has all the information required.
+
+        Returns
+        -------
+        bool
+            True if the order has all required information, False otherwise.
+        """
+        # Check for top level keys
+        required_top_level_keys = {'items', 'payment_info', 'shipping_info'}
+        if not required_top_level_keys.issubset(self._raw_order.keys()):
+            return False
+
+        # Check items
+        if not isinstance(self._raw_order['items'], list) or not self._raw_order['items']:
+            return False
+        
+        for item in self._raw_order['items']:
+            if not isinstance(item, dict):
+                return False
+            if not {'item_id', 'quantity'}.issubset(item.keys()):
+                return False
+            if not isinstance(item['item_id'], int) or not isinstance(item['quantity'], int):
+                return False
+
+        # Check payment_info
+        payment_info = self._raw_order['payment_info']
+        required_payment_keys = {'name', 'card_number', 'expiration_date', 'cvv', 'billing_address'}
+        if not required_payment_keys.issubset(payment_info.keys()):
+            return False
+        
+        # Check billing_address within payment_info
+        billing_address = payment_info['billing_address']
+        required_billing_keys = {'address_1', 'address_2', 'city', 'state', 'zip'}
+        if not required_billing_keys.issubset(billing_address.keys()):
+            return False
+
+        # Check shipping_info
+        shipping_info = self._raw_order['shipping_info']
+        required_shipping_keys = {'name', 'address_1', 'address_2', 'city', 'state', 'zip'}
+        if not required_shipping_keys.issubset(shipping_info.keys()):
+            return False
+
+        return True
+
 
     def __process_order_items__(self):
+        """
+        Processes all the items in the order.
+        Creates a DataFrame of the order with item_id and corresponding quantity.
+        """
         items = self._raw_order['items']
         items_data = {
             CustomerOrderLineItem.customer_order_id.name: [],
@@ -78,6 +184,11 @@ class OrderProcessor(object):
         self._order_items_df = pd.DataFrame(items_data)
 
     def __process_payment__(self):
+        """
+        Process payment information from order. If payment info does not already exist in database,
+        sets `self._payment_df` with `pandas.DataFrame` of the payment info. Also sets `self._payment_info_id`
+        with the existing payment info in database or new one that's going to be inserted.
+        """
         # TODO: This query is really inefficient and will be really slow once the table gets large
         #       Implement a logging in system or something where we'll properly save to a user's account.
         #       Matter fact, payment info probably shouldn't be stored without user permission anyways...
@@ -109,20 +220,25 @@ class OrderProcessor(object):
         # Check if payment info already exists
         id = DatabaseProvider.query_db(self._engine, sql)
         if id:
-            self._payment_info_id = id
+            self._payment_info_id = id[0][0] # query_db returns a list of tuples
             return
         
         # Payment doesn't exist, insert new row
         # Retrieve/calculate new ID for row, since to_sql doesn't return new ID from auto_increment
         id = DatabaseProvider.query_db(self._engine, sa.select(sa.func.ifnull(sa.func.max(PaymentInfo.id), 0) + 1))[0][0]
         
-        df = pd.DataFrame([], columns=[PaymentInfo.id.name] + list(params.keys()))
-        df.loc[0] = [id] + [str(c) for c in params.values()]
+        self._payment_df = pd.DataFrame([], columns=[PaymentInfo.id.name] + list(params.keys()))
+        self._payment_df.loc[0] = [id] + [str(c) for c in params.values()]
 
         self._payment_info_id = id
 
 
     def __process_shipping__(self) -> int:
+        """
+        Process shipping information from order. If shipping info does not already exist in database,
+        sets `self._shipping_df` with `pandas.DataFrame` of the shipping info. Also sets `self._shipping_info_id`
+        with the existing shipping info in database or new one that's going to be inserted.
+        """
         shipment = self._raw_order['shipping_info']        
         sql = sa.select(ShippingInfo.id).where(
             (sa.func.upper(ShippingInfo.name) == sa.func.upper(shipment['name']))
@@ -135,7 +251,7 @@ class OrderProcessor(object):
         
         id = DatabaseProvider.query_db(self._engine, sql)
         if id:
-            self._shipping_info_id = id
+            self._shipping_info_id = id[0][0] # query_db returns a list of tuples
             return
         
         # Payment doesn't exist, insert new row
@@ -143,13 +259,21 @@ class OrderProcessor(object):
         id = DatabaseProvider.query_db(self._engine, sa.select(sa.func.ifnull(sa.func.max(ShippingInfo.id), 0) + 1))[0][0]
 
         # column names are the same as keys in shipment, but uppercase
-        df = pd.DataFrame([], columns=['ID'] + [key.upper() for key in shipment.keys()])
-        df.loc[0] = [id] + [str(c) for c in shipment.values()]
+        self._shipping_df = pd.DataFrame([], columns=[ShippingInfo.id.name] + list(shipment.keys()))
+        self._shipping_df.loc[0] = [id] + [str(c) for c in shipment.values()]
 
         self._shipping_info_id = id
 
     
     def __in_stock__(self) -> bool:
+        """
+        Checks if the items have enough stock quantity based on the order quantity.
+
+        Returns
+        -------
+        bool
+            True if quantity all items in order are <= the corresponding item stock, False otherwise.
+        """
         for item in self._raw_order['items']:
             query = sa.select(Inventory).where(
                 (Inventory.item_id == item['item_id'])
@@ -162,13 +286,29 @@ class OrderProcessor(object):
                 return False
         return True
     
-    def __update_database_with_order__(self):
+    def __update_database_with_order__(self) -> bool:
+        """
+        Makes necessary changes to the database based on the order.
+        Inserts payment and shipping info if they are new.
+        Inserts order and order items.
+        Updates inventory by subtracting the stock quantity by what's ordered.
+
+        Returns
+        -------
+        bool
+            An indicator of success. True if transaction was success, False otherwise.
+        """
         session = Session(self._engine)
+        success = True
         with Session(self._engine) as session:
             session.begin()
             try:
-                session.execute(sa.insert(PaymentInfo).values(self._payment_df.to_dict('records')))
-                session.execute(sa.insert(ShippingInfo).values(self._shipping_df.to_dict('records')))
+                # Shipping and payment may already exist
+                if (self._payment_df is not None and not self._payment_df.empty):
+                    session.execute(sa.insert(PaymentInfo).values(self._payment_df.to_dict('records')))
+                if (self._shipping_df is not None and not self._shipping_df.empty):
+                    session.execute(sa.insert(ShippingInfo).values(self._shipping_df.to_dict('records')))
+
                 session.execute(sa.insert(CustomerOrder).values(self._order_df.to_dict('records')))
                 session.execute(sa.insert(CustomerOrderLineItem).values(self._order_items_df.to_dict('records')))
 
@@ -176,13 +316,15 @@ class OrderProcessor(object):
                     session.execute(
                         sa.update(Inventory).where(
                             Inventory.item_id == row[CustomerOrderLineItem.item_id.name]
-                        ).values(quantity=Inventory.stock_quantity - row[CustomerOrderLineItem.quantity.name])
+                        ).values(stock_quantity=Inventory.stock_quantity - row[CustomerOrderLineItem.quantity.name])
                     )
             except Exception as err:
                 session.rollback()
+                success = False
                 # TODO: Return a message indicating an error occurred making changes to database
             else:
                 session.commit()
+        return success
 
 
         
@@ -196,6 +338,7 @@ order_example = {
         }
     ],
     'payment_info': {
+        'name': 'Jane Doe',
         'card_number': '123',
         'expiration_date': '10/20',
         'cvv': 123,
@@ -208,7 +351,7 @@ order_example = {
         }
     },
     'shipping_info': {
-        'name': 'jane doe',
+        'name': 'Jane Doe',
         'address_1': '123 Main St',
         'address_2': '321 2nd Ave',
         'city': 'Columbus',
